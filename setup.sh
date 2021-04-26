2418ce4043b3d48945cf25c74bb12df8f0d59c3b
Stefanos Eleftheriadis <stelefth@gmail.com>
2021-04-26 19:07:01 +0100

    fixing the script

--- setup.sh
+++ setup.sh
@@ -8,24 +8,25 @@ printf '\n** Preparing install **'
 rm $HOME/.vimrc
 rm $HOME/.gitconfig
 rm -rf $HOME/.config/nvim/
-# rm -rf $HOME/.vim
+rm -rf $HOME/.vim
 
 # Copy conf files (symlinks)
 # $CPY $PWD/zsh/zshrc $HOME/.zshrc
 $CPY $PWD/vimrc $HOME/.vimrc
-# $CPY $PWD/gitconfig $HOME/.gitconfig
+$CPY $PWD/gitconfig $HOME/.gitconfig
 
 # Vim configuration
 printf '\n** Configuring vim **'
 mkdir $HOME/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PlugInstall +qall
+vim +PluginInstall +qall
 
-if [ `which nvim` ];
+NVIM_PATH=`which nvim`
+if [ $NVIM_PATH ];
 then
 	printf '\n** Configuring neo-vim **'
  	mkdir -p $HOME/.config/nvim
 	$CPY $PWD/init.vim $HOME/.config/nvim/init.vim
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
-	vim +PlugInstall +qall
+	vim +PluginInstall +qall
 fi
